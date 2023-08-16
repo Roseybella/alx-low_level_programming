68577d085af3241c786683af496f7a228e27b7c2
Roseybella <roseybella279@gmail.com>
2023-08-15 20:38:53 -0700

    My number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - prints all the numbers of base 16 in lowercase,
+*followed by a new line
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+char c;
+	for (n = 48; n < 57; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
