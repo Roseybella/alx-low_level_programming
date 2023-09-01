c7346b633939a0de8250da2b8f2b1aeb726d3636
Roseybella <roseybella279@gmail.com>
2023-08-31 19:08:45 -0700

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
-#include 'main.h'
+#include "main.h"
 
 /**
-*_print_rev_recursion - print strings in reverse
-* @s: string to reverse
-*/
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
+	if (*s == '\0')
+		return;
+
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	}
 }
